4e5308470efd1072c8e2523ce54e45e3a769acba
Igor <igor.borodin.888@gmail.com>
2022-07-14 17:13:52 +0300

    Task

---
+++ Task001/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 19.напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+
+
+
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+//double SystemCoor(int x1,int y1,int z1,int x2,int y2,int z2){
+// return Math.Sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) + (z1 - z2) * (z1 - z2));
+//}
+//Console.WriteLine("Введите координату x1");
+//int x1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координату y1");
+//int y1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координату z1");
+//int z1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координату x2");
+//int x2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координату y2");
+//int y2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите координату z2");
+//int z2 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(SystemCoor(x1,x2,y1,y2,z1,z2));
+
+//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.Write("input number");
+// int number = Convert.ToInt32( Console.ReadLine());
+// Console.Write(number +"->");
+
+
+
+// for (int i = 1;i<=number;i++)
+// Console.WriteLine(i+"=>"+i*i*i+"");
